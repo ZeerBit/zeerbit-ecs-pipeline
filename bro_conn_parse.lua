2580cca8d093121e8570d43343802417d435e475
Alex Bortok <dev@bortok.com>
2019-12-05 00:03:41 -0800

    network.bytes

--- bro_conn_parse.lua
+++ bro_conn_parse.lua
@@ -14,3 +14,19 @@ function bro_conn_parse_direction(tag, timestamp, record)
   end
   return 1, timestamp, record
 end
+
+function bro_conn_parse_bytes(tag, timestamp, record)
+  local bytes = 0
+  if tonumber(record["source_bytes"]) ~= nil then
+    bytes = bytes + tonumber(record["source_bytes"])
+  end
+  if tonumber(record["destination_bytes"]) ~= nil then
+    bytes = bytes + tonumber(record["destination_bytes"])
+  end
+  if bytes > 0 then
+    record["network_bytes"] = bytes
+    return 1, timestamp, record
+  else
+    return 0, timestamp, record
+  end
+end
