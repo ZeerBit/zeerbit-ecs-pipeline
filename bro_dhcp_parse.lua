cb5f7628bf20a67c08d7eb0445c501378a58bce4
Alex Bortok <dev@bortok.com>
2019-12-17 14:14:07 -0800

    zeek.dhcp.uids instead of session_ids, as an array

--- bro_dhcp_parse.lua
+++ bro_dhcp_parse.lua
@@ -7,6 +7,15 @@ function string:split (sep)
     return fields
 end
 
+function bro_dhcp_parse_uids(tag, timestamp, record)
+  if record["uids"] ~= nil and record["uids"] ~= "-" then
+    record["zeek_dhcp_uids"] = record.uids:split(",")
+    return 1, timestamp, record
+  else
+    return 0, timestamp, record
+  end
+end
+
 function bro_dhcp_parse_msg_types(tag, timestamp, record)
   if record["msg_types"] ~= nil and record["msg_types"] ~= "-" then
     record["zeek_dhcp_msg_types"] = record.msg_types:split(",")
