ced0c88b42408dfe75fe5584982360a90d9a09f0
Alex Bortok <dev@bortok.com>
2020-01-03 14:35:05 -0800

    zeek http_orig arrays

--- bro_http_parse.lua
+++ bro_http_parse.lua
@@ -18,7 +18,23 @@ function bro_http_parse_arrays(tag, timestamp, record)
     record["zeek_http_resp_mime_types"] = record.resp_mime_types:split(",")
   end
 
-  if record["zeek_http_resp_fuids"] ~= nil or record["zeek_http_resp_filenames"] ~= nil or record["zeek_http_resp_mime_types"] ~= nil then
+  if record["orig_fuids"] ~= nil and record["orig_fuids"] ~= "-" then
+    record["zeek_http_orig_fuids"] = record.orig_fuids:split(",")
+  end
+  if record["orig_filenames"] ~= nil and record["orig_filenames"] ~= "-" then
+    record["zeek_http_orig_filenames"] = record.orig_filenames:split(",")
+  end
+  if record["orig_mime_types"] ~= nil and record["orig_mime_types"] ~= "-" then
+    record["zeek_http_orig_mime_types"] = record.orig_mime_types:split(",")
+  end
+
+  if record["zeek_http_resp_fuids"] ~= nil or 
+     record["zeek_http_resp_filenames"] ~= nil or 
+     record["zeek_http_resp_mime_types"] ~= nil or
+     record["zeek_http_orig_fuids"] ~= nil or 
+     record["zeek_http_orig_filenames"] ~= nil or 
+     record["zeek_http_orig_mime_types"] ~= nil 
+     then
     return 1, timestamp, record
   else
     return 0, timestamp, record
