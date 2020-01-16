fd2b8426e012e87fefbd86dae4d2a8a7dfa7a174
Alex Bortok <dev@bortok.com>
2020-01-16 10:08:53 -0800

    json for dhcp

--- bro_dhcp_parse.lua
+++ bro_dhcp_parse.lua
@@ -1,7 +1,7 @@
 require('parse_helpers')
 
 function bro_dhcp_parse_uids(tag, timestamp, record)
-  if record["uids"] ~= nil and record["uids"] ~= "-" then
+  if record["uids"] ~= nil and type(record["uids"]) == "string" and record["uids"] ~= "-" then
     record["zeek_dhcp_uids"] = record.uids:split(",")
     return 1, timestamp, record
   else
@@ -10,7 +10,7 @@ function bro_dhcp_parse_uids(tag, timestamp, record)
 end
 
 function bro_dhcp_parse_msg_types(tag, timestamp, record)
-  if record["msg_types"] ~= nil and record["msg_types"] ~= "-" then
+  if record["msg_types"] ~= nil and type(record["msg_types"]) == "string" and record["msg_types"] ~= "-" then
     record["zeek_dhcp_msg_types"] = record.msg_types:split(",")
     return 1, timestamp, record
   else
