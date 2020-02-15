31db90254278ac24b3a8d93890d08a8e64396541
Alex Bortok <dev@bortok.com>
2020-02-15 10:20:26 -0800

    autoprefix http.log

--- bro_http_parse.lua
+++ bro_http_parse.lua
@@ -4,10 +4,14 @@ function get_file_extension(url)
   return url:match("^.+%.(.+)$")
 end
 
+function bro_http_prefix_all(tag, timestamp, record)
+  return 1, timestamp, record_prefix_all(record, "zeek_http_")
+end
+
 function bro_http_parse_uri(tag, timestamp, record)
   local uri = {};
-  if record["uri"] ~= nil and record["uri"] ~= "-" then
-    uri = record.uri:split("?")
+  if record["zeek_http_uri"] ~= nil and record["zeek_http_uri"] ~= "-" then
+    uri = record.zeek_http_uri:split("?")
   end
   
   record["url_path"] = uri[1]
@@ -34,35 +38,19 @@ end
 
 -- Only called when parsing tab log files, no need to account for getting original values as tables from json
 function bro_http_parse_arrays(tag, timestamp, record)
-  if record["resp_fuids"] ~= nil and record["resp_fuids"] ~= "-" then
-    record["zeek_http_resp_fuids"] = record.resp_fuids:split(",")
-  end
-  if record["resp_filenames"] ~= nil and record["resp_filenames"] ~= "-" then
-    record["zeek_http_resp_filenames"] = record.resp_filenames:split(",")
-  end
-  if record["resp_mime_types"] ~= nil and record["resp_mime_types"] ~= "-" then
-    record["zeek_http_resp_mime_types"] = record.resp_mime_types:split(",")
-  end
+  record["zeek_http_resp_fuids"]      = variable_to_table(record["zeek_http_resp_fuids"],",")
+  record["zeek_http_resp_filenames"]  = variable_to_table(record["zeek_http_resp_filenames"],",")
+  record["zeek_http_resp_mime_types"] = variable_to_table(record["zeek_http_resp_mime_types"],",")
 
-  if record["orig_fuids"] ~= nil and record["orig_fuids"] ~= "-" then
-    record["zeek_http_orig_fuids"] = record.orig_fuids:split(",")
-  end
-  if record["orig_filenames"] ~= nil and record["orig_filenames"] ~= "-" then
-    record["zeek_http_orig_filenames"] = record.orig_filenames:split(",")
-  end
-  if record["orig_mime_types"] ~= nil and record["orig_mime_types"] ~= "-" then
-    record["zeek_http_orig_mime_types"] = record.orig_mime_types:split(",")
-  end
+  record["zeek_http_orig_fuids"]      = variable_to_table(record["zeek_http_orig_fuids"],",")
+  record["zeek_http_orig_filenames"]  = variable_to_table(record["zeek_http_orig_filenames"],",")
+  record["zeek_http_orig_mime_types"] = variable_to_table(record["zeek_http_orig_mime_types"],",")
   
-  if record["tags"] ~= nil and record["tags"] ~= "(empty)" then
-    record["zeek_http_tags"] = record.tags:split(",")
-  end
+  record["zeek_http_tags"]            = variable_to_table(record["zeek_http_tags"],",")
   
   -- To test 'proxied' parsing in the environment w/o a proxy, add this to $PREFIX/share/bro/site/local.bro
   -- redef HTTP::proxy_headers += { "ACCEPT-ENCODING", "ACCEPT-LANGUAGE" };
-  if record["proxied"] ~= nil and record["proxied"] ~= "-" then
-    record["zeek_http_proxied"] = record.proxied:split(",")
-  end
+  record["zeek_http_proxied"]         = variable_to_table(record["zeek_http_proxied"],",")
 
   if record["zeek_http_resp_fuids"] ~= nil or 
      record["zeek_http_resp_filenames"] ~= nil or 
