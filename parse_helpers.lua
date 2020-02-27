66fca20c202222b0a191cddb74be40c1a48b686f
Alex Bortok <dev@bortok.com>
2020-02-26 22:27:06 -0800

    parse lon/lat from conn-add-geo

--- parse_helpers.lua
+++ parse_helpers.lua
@@ -5,6 +5,12 @@ function string:split (sep)
     return fields
 end
 
+function table_size(table)
+  local count = 0
+  for _ in pairs(table) do count = count + 1 end
+  return count
+end
+
 function variable_to_table(var, sep)
   if type(var) == 'string' then
     return var:split(sep)
