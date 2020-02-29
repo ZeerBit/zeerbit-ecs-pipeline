11754c77636769bc1d4bfb91e275138926039872
Alex Bortok <dev@bortok.com>
2020-02-29 09:41:37 -0800

    Populate host.ip and host.hostname from dhcp.log
    Elastic SIEM is a primary target for this

--- bro_dhcp_parse.lua
+++ bro_dhcp_parse.lua
@@ -25,3 +25,20 @@ function bro_dhcp_parse_msg_types(tag, timestamp, record)
     return 1, timestamp, record
   end
 end
+
+-- If zeek_dhcp_client_fqdn (DHCP Option 81) is empty, use host.hostname + "." + host.domain to populate the field
+function bro_dhcp_populate_missing_host_name(tag, timestamp, record)
+  if (record["host_name"] == nil or record["host_name"] == "-") then
+    if (record["host_hostname"] ~= nil and record["host_hostname"] ~= "-") then
+      record["host_name"] = record["host_hostname"]
+      if (record["host_domain"] ~= nil and record["host_domain"] ~= "-") then
+        record["host_name"] = record["host_name"].."."..record["host_domain"]
+      end
+      return 1, timestamp, record
+    else
+      return 0, timestamp, record
+    end
+  else
+    return 0, timestamp, record
+  end
+end
