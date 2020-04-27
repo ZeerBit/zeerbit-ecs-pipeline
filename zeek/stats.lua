a20dbff34442929695aa28560e6ffaa7428f4461
Alex Bortok <dev@bortok.com>
2020-04-26 22:44:37 -0700

    Added zeek.stats.mem_bytes

--- zeek/stats.lua
+++ zeek/stats.lua
@@ -3,3 +3,18 @@ require('parse_helpers')
 function zeek_stats_prefix_all(tag, timestamp, record)
   return 1, timestamp, record_prefix_all(record, "zeek_stats_")
 end
+
+function zeek_stats_add_mem_bytes(tag, timestamp, record)
+  -- Stats::Info[mem] Amount of memory currently in use in MB.
+  local conversion_rate = 1024 * 1024
+  local mem_bytes = 0
+  if record["zeek_stats_mem"] ~= nil then
+    mem_bytes = tonumber(record["zeek_stats_mem"]) * conversion_rate
+  end
+  if mem_bytes > 0 then
+    record["zeek_stats_mem_bytes"] = mem_bytes
+    return 1, timestamp, record
+  else
+    return 0, timestamp, record
+  end
+end
